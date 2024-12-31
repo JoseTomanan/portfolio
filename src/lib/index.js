3bbb6df15190f25fff72021cded95899abab0be1
HoweZae <jdtomanan@up.edu.ph>
2024-12-31 12:46:20 +0800

    feat: Change GitHub link

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
 export const link = {
     li: "https://www.linkedin.com/in/jedtomanan/",
-    gh: "https://github.com/HoweZae",
+    gh: "https://github.com/JoseTomanan",
     fb: "https://www.facebook.com/jedtomanan/",
     ig: "https://www.instagram.com/jose.tmnn/",
     twt: "https://x.com/howe_zae",
