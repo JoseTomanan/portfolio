1532a8a235de7359db936cff37a0fbc4737c1bee
HoweZae <jdtomanan@up.edu.ph>
2025-01-05 22:14:42 +0800

    fix: Wording in Shogimon

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -45,12 +45,12 @@
                 id="proj0"
                 href="_blank"
                 title="Shogimon"
-                subtitle="A Pokemon-inspired Shogi game using Python"
+                subtitle="A Pokemon-inspired Shogi-esque video game using Python"
                 date="DEC 2024 - JAN 2025"
                 other="
                     A Model-View-Controller (MVC) structured simple video game created using Python video game package Pygame.
-                    Utilizes industry-ready and collaboration-ready software development practices such as the SOLID principles, the observer pattern, and networking using message passing.
-                    Implements both online (networking using Go as the server connector) and offline (pass-and-play) mode.
+                    Utilizes collaboration-ready software development practices such as SOLID principles, design patterns, and message passing between components.
+                    Implements both online (networking) and offline (pass-and-play) mode.
                     Made in compliance for the requirements of course CS 150: Programming Languages.
                 "
                 techInvolved={ ["Python", "Go"] }
