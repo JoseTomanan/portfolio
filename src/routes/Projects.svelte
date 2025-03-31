9d14097a17b9857e747e6d5cb1628e2e9c8fcc0a
HoweZae <jdtomanan@up.edu.ph>
2025-03-31 12:32:16 +0800

    docs: Indentation

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -14,73 +14,73 @@
     <div class="flex flex-col gap-2 mx-2">
       <!-- 
       <Article 
-      id="proj0"
-      href="_blank"
-      title="Food"
-      subtitle="Food app"
-      date="AUG 2024 - PRESENT"
-      other="
-      ,,,
-      Made in compliance for the requirements of course CS 191 and 192: Software Engineering I and II.
-      "
-      techInvolved={ ["Svelte", "PHP"] }
-      on:click|once={ () => linkClicked("proj0") }
+        id="proj0"
+        href="_blank"
+        title="Food"
+        subtitle="Food app"
+        date="AUG 2024 - PRESENT"
+        other="
+            ,,,
+            Made in compliance for the requirements of course CS 191 and 192: Software Engineering I and II.
+          "
+        techInvolved={ ["Svelte", "PHP"] }
+        on:click|once={ () => linkClicked("proj0") }
       /> 
       -->
       <Article 
-      id="proj1"
-      href="https://github.com/HoweZae/portfolio"
-      title="JoseTomanan.io"
-      subtitle="Personal portfolio-resume (i.e., this webpage!)"
-      date="JUL 2024 - PRESENT"
-      other="
-                    A static single-page website that showcases the relevant skills I have developed throughout my learning journey.
-                    Also created to demonstrate front-end tech learned and to practice my UI/UX design choices.
-                    Written using Svelte and Tailwind, to prioritize the webpage's responsiveness and intuitiveness.
-                    Used supplementary JavaScript and CSS to add more complex functionality.
-                "
-      techInvolved={ ["Svelte", "Tailwind", "JavaScript", "CSS"] }
+        id="proj1"
+        href="https://github.com/HoweZae/portfolio"
+        title="JoseTomanan.io"
+        subtitle="Personal portfolio-resume (i.e., this webpage!)"
+        date="JUL 2024 - PRESENT"
+        other="
+            A static single-page website that showcases the relevant skills I have developed throughout my learning journey.
+            Also created to demonstrate front-end tech learned and to practice my UI/UX design choices.
+            Written using Svelte and Tailwind, to prioritize the webpage's responsiveness and intuitiveness.
+            Used supplementary JavaScript and CSS to add more complex functionality.
+          "
+        techInvolved={ ["Svelte", "Tailwind", "JavaScript", "CSS"] }
       />
       <Article 
-      id="proj0"
-      href="_blank"
-      title="Shogimon"
-      subtitle="A Pokemon-inspired Shogi-esque video game using Python"
-      date="DEC 2024 - JAN 2025"
-      other="
-                    A Model-View-Controller (MVC) structured simple video game created using Python video game package Pygame.
-                    Employs collaboration-ready programming practices such as the SOLID principles, design patterns, and message passing between components.
-                    Implements both online (networking) and offline (pass-and-play) mode.
-                    Made in compliance for the requirements of course CS 150: Programming Languages.
-                "
-      techInvolved={ ["Python", "Go"] }
+        id="proj0"
+        href="_blank"
+        title="Shogimon"
+        subtitle="A Pokemon-inspired Shogi-esque video game using Python"
+        date="DEC 2024 - JAN 2025"
+        other="
+            A Model-View-Controller (MVC) structured simple video game created using Python video game package Pygame.
+            Employs collaboration-ready programming practices such as the SOLID principles, design patterns, and message passing between components.
+            Implements both online (networking) and offline (pass-and-play) mode.
+            Made in compliance for the requirements of course CS 150: Programming Languages.
+          "
+        techInvolved={ ["Python", "Go"] }
       /> 
       <Article 
-      id="proj2"
-      href="https://tes-birth-rate.vercel.app"
-      title="Poverty and Parenting: A Data Science Study"
-      subtitle="Economic analysis of birth rates in the Philippines"
-      date="FEB 2024 - JUN 2024"
-      other="
-                    A visual exploration study, done using data science Python packages: Pandas, Seaborn, Geopandas.
-                    Uses data science concepts to find an association between registered live births and poverty incidence in each municipality, as well as data visualization techniques to make the results intuitive and easily digestible.
-                    Conducted in compliance for the requirements of course CS 132: Data Science.
-                "
-      techInvolved={ ["Python"] }
+        id="proj2"
+        href="https://tes-birth-rate.vercel.app"
+        title="Poverty and Parenting: A Data Science Study"
+        subtitle="Economic analysis of birth rates in the Philippines"
+        date="FEB 2024 - JUN 2024"
+        other="
+            A visual exploration study, done using data science Python packages: Pandas, Seaborn, Geopandas.
+            Uses data science concepts to find an association between registered live births and poverty incidence in each municipality, as well as data visualization techniques to make the results intuitive and easily digestible.
+            Conducted in compliance for the requirements of course CS 132: Data Science.
+          "
+        techInvolved={ ["Python"] }
       />
       <Article 
-      id="proj3"
-      href="https://github.com/JoseTomanan/finance-tracker-automator"
-      title="Finance Tracker Automator"
-      subtitle="Automation script for Google Sheets using Google AppsScript API"
-      date="FEB 2023 - PRESENT"
-      other="
-                    A scripting project that I use for my personal spreadsheet to track my daily outgoing and incoming funds.
-                    Uses the API to automate the addition of new entries, separation of each week, and creation of a new sheet for each new month.
-                    Written in TypeScript, later transpiled into JavaScript through Clasp (for Google AppsScript).
-                    Implements the SOLID principles and design patterns learned in course CS 150: Programming Languages.
-                "
-      techInvolved={ ["Google Sheets", "Google Apps Script", "TypeScript"] }
+        id="proj3"
+        href="https://github.com/JoseTomanan/finance-tracker-automator"
+        title="Finance Tracker Automator"
+        subtitle="Automation script for Google Sheets using Google AppsScript API"
+        date="FEB 2023 - PRESENT"
+        other="
+            A scripting project that I use for my personal spreadsheet to track my daily outgoing and incoming funds.
+            Uses the API to automate the addition of new entries, separation of each week, and creation of a new sheet for each new month.
+            Written in TypeScript, later transpiled into JavaScript through Clasp (for Google AppsScript).
+            Implements the SOLID principles and design patterns learned in course CS 150: Programming Languages.
+          "
+        techInvolved={ ["Google Sheets", "Google Apps Script", "TypeScript"] }
       />
     </div>
     
