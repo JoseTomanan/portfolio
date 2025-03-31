0f3b6a3a4f332d2089ad3bbf04d48215aec1773b
HoweZae <jdtomanan@up.edu.ph>
2025-03-31 12:09:59 +0800

    docs: Reindent

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,44 +1,44 @@
 <script>
-    import "../app.css";
+  import "../app.css";
 </script>
 
 <div class="app bg-fixed">
-    <nav class="
+  <nav class="
         flex items-center justify-center gap-8
         fixed w-full max-w-screen p-4 z-50
         bg-white backdrop-blur-sm opacity-90
         shadow-xl
         border-b-2 border-none"
-    >
-        <a class="justify-items-start font-bold italic" href="#top">JoseTomanan.io</a>
-            <a href="#me">Me</a>
-            <a href="#roles">Roles</a>
-            <a href="#projects">Projects</a>
-            <!-- <a href="#hobbies">Hobbies</a> -->
-            <a href="#contact">Contact</a>
-    </nav>
+  >
+  <a class="justify-items-start font-bold italic" href="#top">JoseTomanan.io</a>
+  <a href="#me">Me</a>
+  <a href="#roles">Roles</a>
+  <a href="#projects">Projects</a>
+  <!-- <a href="#hobbies">Hobbies</a> -->
+  <a href="#contact">Contact</a>
+</nav>
 
-    <premain class="flex justify-center">
-        <main class="mx-2 min-h-screen max-w-[960px] flex flex-col gap-4">
-            <slot />
-        </main>
-    </premain>
+<premain class="flex justify-center">
+  <main class="mx-2 min-h-screen max-w-[960px] flex flex-col gap-4">
+    <slot />
+  </main>
+</premain>
 
-    <footer class="py-2">
-        <h6 class="text-center text-slate-400">
-            //
-            This page is written with Svelte and Tailwind.
-            Feel free to open the codebase to see how I made this!
-        </h6>
-    </footer>
+<footer class="py-2">
+  <h6 class="text-center text-slate-400">
+    //
+    This page is written with Svelte and Tailwind.
+    Feel free to open the codebase to see how I made this!
+  </h6>
+</footer>
 </div>
 
 <style>
-    nav > a {
-        @apply transition ease-out;
-    }
-
-    nav > a:hover {
-        @apply text-sky-500;
-    }
+  nav > a {
+    @apply transition ease-out;
+  }
+  
+  nav > a:hover {
+    @apply text-sky-500;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,41 @@
 <script>
-    import Top from "./Top.svelte";
-    import Me from "./Me.svelte";
-    import Roles from "./Roles.svelte";
-    import Projects from "./Projects.svelte";
-    import Contact from "./Contact.svelte";
-
-    import { onMount } from "svelte";
-    // import { fade } from "svelte/transition";
-    // import { tweened, spring } from "svelte/motion";
-    // import { elasticOut } from "svelte/easing";
-
-    let startPage = false;
-
-    onMount(() => {
-        startPage = true;
-    });
+  import Top from "./Top.svelte";
+  import Me from "./Me.svelte";
+  import Roles from "./Roles.svelte";
+  import Projects from "./Projects.svelte";
+  import Contact from "./Contact.svelte";
+  
+  import { onMount } from "svelte";
+  // import { fade } from "svelte/transition";
+  // import { tweened, spring } from "svelte/motion";
+  // import { elasticOut } from "svelte/easing";
+  
+  let startPage = false;
+  
+  onMount(() => {
+    startPage = true;
+  });
 </script>
 
 <svelte:head>
-    <title>Jose Tomanan</title>
-    <meta name="description" content="Jose's portfolio" />
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/site.webmanifest">
+<title>Jose Tomanan</title>
+<meta name="description" content="Jose's portfolio" />
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 </svelte:head>
 
 <Top />
 
 {#if startPage}
-    <Me />
-    
-    <Roles />
-    
-    <Projects />
-    
-    <Contact />
+<Me />
+
+<Roles />
+
+<Projects />
+
+<Contact />
 {/if}
 
 <style>
