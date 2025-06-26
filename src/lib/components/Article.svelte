df6c479c7863c46c62afe31529389f23401d0c76
HoweZae <jdtomanan@up.edu.ph>
2025-06-26 21:57:50 +0800

    feat: Spacing

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -14,20 +14,21 @@
 </script>
 
 <article class="
-  flex gap-4 items-baseline
-  p-4 rounded-lg text-left border
-  bg-lucy-surface border-lucy-border
-  hover:bg-lucy-surface-alt hover:border-lucy-border-alt hover:shadow-lucy-border-alt hover:shadow-sm
-  transition ease-linear z-10
-">
+    flex gap-4 items-baseline
+    p-4 rounded-lg text-left border
+    bg-lucy-surface border-lucy-border
+    hover:bg-lucy-surface-alt hover:border-lucy-border-alt
+    hover:shadow-lucy-border-alt hover:shadow-sm
+    transition ease-linear z-10
+  ">
   <date class="grow text-right text-sm font-mono tracking-tighter text-lucy-muted transition ease-out">
     {date}
   </date>
   
-  <div class="basis-5/6 md:basis-3/4 flex flex-col gap-0.5">
-    <div class="grid">
-      <h3 class="text-lg underline-offset-2 flex font-sans font-semibold text-lucy-fg hover:text-lucy-secondary not-italic">
-        <a class="flex items-baseline clickableLink gap-2 hover:gap-4 transition-all ease-out" target="_blank" {id} {href} on:click|once>
+  <div class="basis-5/6 md:basis-3/4 flex flex-col gap-2">
+    <div class="grid leading-3">
+      <h3 class="text-lg underline-offset-2 flex font-sans font-semibold not-italic">
+        <a class="flex items-baseline gap-2 hover:gap-4 text-lucy-fg/90 hover:text-lucy-secondary transition-all ease-out" target="_blank" {id} {href} on:click|once>
           {title}
           <svg
           id="link" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
@@ -37,14 +38,14 @@
           </svg>
         </a>
       </h3>
-      <h5 class="text-lucy-fg/90 mt-0">{subtitle}</h5>
+      <h5 class="text-lucy-fg/80 mt-0">{subtitle}</h5>
     </div>
 
-    <p class="text-sm grow tracking-tight">
+    <p class="text-sm grow leading-4">
       {other}
     </p>
 
-    <techs class="flex gap-1 mt-1.5">
+    <techs class="flex gap-1">
       <!-- TODO: justify between, make sure this is at the bottom. mt-1.5 is not it -->
       {#each techInvolved as tech}
         <perTech class="bg-lucy-surface-alt rounded-xl px-2 text-lucy-muted flex items-center transition ease-out">
@@ -57,7 +58,7 @@
 
 <style>
   article:hover h3 {
-    /* text-decoration-line: underline; */
+    @apply text-lucy-fg;
     @apply transition-all duration-150 ease-in-out;
   }
 
