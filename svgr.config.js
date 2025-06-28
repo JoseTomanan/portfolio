762424f2c2fcd7ea4aa123361b5d753145f4bc22
Jose Tomanan <jdtomanan@up.edu.ph>
2025-06-28 21:56:39 +0800

    chore: Init config

---
+++ svgr.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dimensions: false,
+    icon: true,
+    svgProps: {
+        fill: 'currentColor',
+    },
+    replaceAttrValues: {
+        '#000': 'currentColor',
+        '#000000': 'currentColor',
+        '#fff': 'currentColor',
+        '#ffffff': 'currentColor',
+    },
+};
