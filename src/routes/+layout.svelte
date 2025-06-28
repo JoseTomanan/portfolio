92faa14819a2e7754302e4328c329064fef81942
Jose Tomanan <jdtomanan@up.edu.ph>
2025-06-28 15:03:50 +0800

    feat: Make resume hover different

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,11 +10,13 @@
       gap-4 md:gap-16
       focus:border-none active:border-none
     ">
-  <a class="justify-items-start font-bold italic font-heading hoverable-special pr-0 md:pr-4" href="#top">JoseTomanan.io</a>
+  <a id="first" class="justify-items-start font-bold italic font-heading pr-0 md:pr-4" href="#top">JoseTomanan.io</a>
   <a href="#me">Me</a>
   <a href="#roles">Roles</a>
   <a href="#projects">Projects</a>
-  <a href="./resume.pdf" target="_blank">Resume</a>
+  <a href="./resume.pdf" class="hoverable-special" target="_blank">
+    Resume
+  </a>
   <!-- <a href="#contact">Contact</a> -->
 </nav>
 
@@ -37,9 +39,13 @@
 
 <style>
   nav > a {
-    @apply transition ease-out text-lucy-muted hover:text-lucy-primary;
+    @apply transition ease-out text-lucy-muted hover:text-lucy-tertiary;
   }
   
+  nav > a#first {
+    @apply hover:text-lucy-primary;
+  }
+
   nav > a.hoverable-special {
     @apply hover:text-lucy-secondary;
   }
