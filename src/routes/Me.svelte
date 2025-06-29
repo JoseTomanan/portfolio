9567233b0204a5b3c5875c3852cc444017526d61
Jose Tomanan <jdtomanan@up.edu.ph>
2025-06-29 15:17:22 +0800

    feat: Edit content; Make better highlighting

--- src/routes/Me.svelte
+++ src/routes/Me.svelte
@@ -5,32 +5,31 @@
 <jumpable id="me" />
 <page class="h-auto gap-0" transition:fly={{ delay: 100, duration: 1000 }}>
   <section class="flex flex-col gap-2 pb-4 rounded-b-none border-b border-lucy-border">
-    <h2>What do I do?</h2>
+    <h2>Who am I?</h2>
     <p>
-      As a developer, I appreciate the philosophy of <span class="hoverable-link">code craftsmanship</span>, i.e. the use of sustainable design patterns and tech principles to build maintainable codebases.
-      I've applied this in frontend, creating web designs using Svelte and Tailwind, as well as in backend, building APIs with Django and Spring Boot.
-    </p>
-    <p>
-      I bring these principles into my projects, as I feel that this is ultimately what makes an application's codebase "timeless": its intuitiveness.
+      As a developer, I thrive in the intersection of frontend and backend.
+      I have put myself in both shoes&mdash;creating clean UIs with SvelteKit and building solid APIs with Django and Spring Boot.
+      These experiences have taught me to aim for detail-driven solutions, resulting in products with codebases that are
+      <span class="minor-highlight">as thoughtful as they are functional</span>.
     </p>
   </section>
   
   <section class="flex flex-col gap-2 rounded-t-none pt-4 ">
     <p>
-      Outside of work, I prefer to take a break from all the screen time by spending my free time away from the keyboard &mdash;
-      I do this by <span class="hoverable-link">being physically active and keeping fit</span>, usually through weightlifting, playing basketball, or running.
+      Outside of work, I prefer to take a break from all the screen time by spending my breaks away from the keyboard &mdash;
+      I do this by <span class="minor-highlight">being physically active and keeping fit</span>, usually through weightlifting, playing basketball, or running.
     </p>
     
     <p>
       I am also an 
-      <a href="https://www.instagram.com/p/B6p4rR_hoUC/" class="hoverable-link" target="_blank">avid Swiftie</a>: 
+      <a href="https://www.instagram.com/p/B6p4rR_hoUC/" class="hoverable-link cursor-help" target="_blank">avid Swiftie</a>: 
       I'd bet my left leg I know more about Taylor Swift than you!
     </p>
   </section>
 </page>
 
 <style>
-  a.hoverable-link {
-    @apply cursor-help;
+  span.minor-highlight {
+    @apply font-medium text-lucy-fg hover:text-lucy-tertiary transition ease-linear;
   }
 </style>
