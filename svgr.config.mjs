38b1eacd27863a09abcb04f2ae5e1c3b71eda1dc
HoweZae <jdtomanan@up.edu.ph>
2025-07-02 20:46:15 +0800

    feat: Add config for svgr

---
+++ svgr.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  icon: true,
+  svgProps: {
+    fill: 'currentColor',
+  },
+  replaceAttrValues: {
+    '#000': 'currentColor',
+    '#fff': 'currentColor',
+    '#000000': 'currentColor',
+    '#ffffff': 'currentColor',
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import svgr from 'vite-plugin-svgr';
+import svgrConfig from './svgr.config.mjs';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
-		svgr(),
+		svgr({ svgrOptions: svgrConfig }),
 	]
 });
