8af0eedcb94f05b1bec1e483cb22e5c10b5b1529
HoweZae <jdtomanan@up.edu.ph>
2025-07-02 20:49:57 +0800

    chore: Remove svgr entirely

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
-import svgr from 'vite-plugin-svgr';
-import svgrConfig from './svgr.config.mjs';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [
 		enhancedImages(),
 		sveltekit(),
-		svgr({ svgrOptions: svgrConfig }),
 	]
 });
