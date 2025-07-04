e6910c1caeaef53fcbd8f82274f4570cdcdececd
HoweZae <jdtomanan@up.edu.ph>
2025-07-04 22:50:48 +0800

    chore: Remove obsolete icons

--- src/lib/components/icons/ArrowIcon.svelte
+++
@@ -1,6 +0,0 @@
-<svg
-  height="100%" width="100%" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" class="{$$props.class}"
-  >
-  <line x1="7" y1="17" x2="17" y2="7"></line>
-  <polyline points="7 7 17 7 17 17"></polyline>
-</svg>
