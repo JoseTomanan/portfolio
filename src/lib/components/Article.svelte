5271f72fb1f1e9a88b11bca9ef5a03a5e36922b3
HoweZae <jdtomanan@up.edu.ph>
2025-07-05 22:33:48 +0800

    feat!: Use icons instead of names for techs

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -14,20 +14,38 @@
 
 <article class="
     group/whole
-    flex gap-4 items-baseline
-    p-4 rounded-lg text-left border
+    flex gap-4 items-baseline md:p-4 rounded-lg text-left border
     bg-lucy-surface border-lucy-border
     hover:bg-lucy-surface-alt hover:border-lucy-border-alt
-    hover:shadow-lucy-border-alt hover:shadow-sm
     transition ease-linear z-10
   ">
-  <date class="
-      grow text-right text-sm font-mono tracking-tighter text-lucy-muted
-      group-hover/whole:text-lucy-fg/100
-      transition ease-out
-    ">
-    {date}
-  </date>
+
+  <div class="basis-1/6 md:basis-1/4 flex flex-col justify-start h-full">
+    <span class="
+        text-right text-sm font-mono tracking-tighter transition ease-out
+        text-lucy-muted
+        group-hover/whole:text-lucy-fg/100
+      ">
+      {date}
+    </span>
+
+    <div class="flex-1" />
+
+    <div class="flex flex-row justify-end w-full">
+      {#each techInvolved as tech}
+        <div class="
+            flex items-center transition ease-out p-0.5
+            text-lucy-muted/80
+            group-hover/whole:text-lucy-muted/100
+          ">
+          <Icon
+              icon={ (tech != "") ? tech : "mdi:question-mark-circle" }
+              class="h-4 w-4"
+            />
+        </div>
+      {/each}
+    </div>
+  </div>
   
   <div class="basis-5/6 md:basis-3/4 flex flex-col gap-2">
     <div class="grid leading-4">
@@ -45,7 +63,7 @@
           {title}
           <Icon
               icon="ic:baseline-arrow-outward"
-              class="w-4 h-4 opacity-0 group-hover/link:opacity-100"
+              class="w-4 h-4 opacity-0 group-hover/whole:opacity-100"
             />
         </a>
       </h3>
@@ -58,27 +76,10 @@
     </div>
 
     <p class="
-        text-sm grow leading-5 tracking-tight text-lucy-muted/75
-        group-hover/whole:text-lucy-muted/100
+        text-base grow leading-5 tracking-tighter text-lucy-muted/50
+        group-hover/whole:text-lucy-muted/80
       ">
       {other}
     </p>
-
-    <techs class="flex gap-1">
-      <!-- TODO: justify between, make sure this is at the bottom. mt-1.5 is not it -->
-      {#each techInvolved as tech}
-        <perTech class="
-            flex items-center transition ease-out rounded-xl px-2
-            bg-lucy-surface-alt text-lucy-muted
-            group-hover/whole:bg-lucy-hover group-hover/whole:text-lucy-fg
-            hover:bg-lucy-tertiary hover:text-lucy-surface-alt
-          ">
-          { tech }
-        </perTech>
-      {/each}
-    </techs>
   </div>
 </article>
-
-<style>
-</style>
