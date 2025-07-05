edd9cd08098d98f6c6e6e0cf7f6dab05dc3d698d
HoweZae <jdtomanan@up.edu.ph>
2025-07-05 20:11:03 +0800

    feat!: Use iconify for arrow icon

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import Icon from "@iconify/svelte"
+
   export let id: string
   export let href: string
   // export let color;
@@ -25,14 +27,17 @@
   <div class="basis-5/6 md:basis-3/4 flex flex-col gap-2">
     <div class="grid leading-4">
       <h3 class="text-lg underline-offset-2 flex font-sans font-semibold not-italic">
-        <a class="flex items-baseline gap-2 hover:gap-4 text-lucy-fg/90 hover:text-lucy-secondary transition-all ease-out" target="_blank" {id} {href} on:click|once>
+        <a class="
+            group flex items-baseline gap-2 hover:gap-4
+            text-lucy-fg/90 hover:text-lucy-secondary
+            transition-all ease-out
+          " target="_blank" {id} {href} on:click|once
+          >
           {title}
-          <svg
-              id="link" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round"
-            >
-            <line x1="7" y1="17" x2="17" y2="7"></line>
-            <polyline points="7 7 17 7 17 17"></polyline>
-          </svg>
+          <Icon
+              icon="ic:baseline-arrow-outward"
+              class="w-4 h-4 opacity-50 group-hover:opacity-100"
+            />
         </a>
       </h3>
       <h5 class="text-lucy-fg/80 mt-0">{subtitle}</h5>
