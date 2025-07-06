ac474e804568bca0db73a2a6c46514a40a390488
HoweZae <jdtomanan@up.edu.ph>
2025-07-06 17:24:26 +0800

    init: Init Shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./src/**/*.{html,js,svelte}"],
+  darkMode: ["class"],
+  safelist: ["dark"],
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     fontFamily: {
       'heading': ["IBM Plex Sans", "sans-serif"],
       'sans': ["Inter", "sans-serif"],
       'mono': ["JetBrains Mono", "monospace"],
     },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        xs: "475px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        xxl: "1536px",
+      },
+    },
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
+        border: "hsl(var(--border) / <alpha-value>)",
+        input: "hsl(var(--input) / <alpha-value>)",
+        ring: "hsl(var(--ring) / <alpha-value>)",
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
         lucy: {
           bg: '#100F13',
           surface: {
@@ -29,6 +46,44 @@ export default {
           },
           hover: '#686069',
         },
+        primary: {
+          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+          foreground: "hsl(var(--primary-foreground) / <alpha-value>)"
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+          foreground: "hsl(var(--secondary-foreground) / <alpha-value>)"
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)"
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+          foreground: "hsl(var(--muted-foreground) / <alpha-value>)"
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+          foreground: "hsl(var(--accent-foreground) / <alpha-value>)"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+          foreground: "hsl(var(--popover-foreground) / <alpha-value>)"
+        },
+        card: {
+          DEFAULT: "hsl(var(--card) / <alpha-value>)",
+          foreground: "hsl(var(--card-foreground) / <alpha-value>)"
+        }
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      },
+      fontFamily: {
+        'heading': ["IBM Plex Sans", "sans-serif"],
+        'sans': ["Inter", "sans-serif"],
+        'mono': ["JetBrains Mono", "monospace"],
       },
     },
     screens: {
@@ -40,6 +95,5 @@ export default {
       xxl: "1536px",
     },
   },
-  plugins: [],
 }
 
