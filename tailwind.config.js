782779afde6fdb7f77389a29726d4d302c5d5df1
HoweZae <jdtomanan@up.edu.ph>
2025-07-07 21:10:59 +0800

    chore: Remove obsolete

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,14 +13,6 @@ export default {
     container: {
       center: true,
       padding: "2rem",
-      screens: {
-        xs: "475px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        xxl: "1536px",
-      },
     },
     extend: {
       colors: {
@@ -73,7 +65,7 @@ export default {
         card: {
           DEFAULT: "hsl(var(--card) / <alpha-value>)",
           foreground: "hsl(var(--card-foreground) / <alpha-value>)"
-        }
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
