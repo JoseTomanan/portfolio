8e8209c64bdc07fc249470c708e06d1bcc537c3e
HoweZae <jdtomanan@up.edu.ph>
2025-07-22 17:37:50 +0800

    chore: spacing

--- src/routes/Projects.svelte
+++ src/routes/Projects.svelte
@@ -10,91 +10,80 @@
     <h1>What projects have I made?</h1>
     
     <div class="flex flex-col gap-6">
-      <Article 
-        id="proj4"
-        href="https://github.com/JoseTomanan/cs192-lutong-bahay"
-        title="Lutong Bahay"
-        subtitle="Full-stack app/database for Filipino recipes"
-        date="AUG 2024 - MAY 2025"
-        other="
-            A database web app with fully functional search filters/sorting functions and user authentication system.
-            Also includes an administrator mode for adding/editing recipes and suspending users.
-            Written using Svelte and Tailwind (frontend), and Django-REST framework (backend), with the database hosted in Supabase.
-            Part of a 5-man, year long project done in compliance for the requirements of course CS 191 and 192: Software Engineering I and II.
-          "
-        techInvolved={ ["simple-icons:Svelte", "simple-icons:TailwindCSS", "simple-icons:Django", "simple-icons:Supabase"] }
-      />
-			<Article 
-        id="proj4"
-        href="https://potterdex.vercel.app"
-        title="PotterDex"
-        subtitle="React-based Harry Potter database interface"
-        date="DEC 2024 - JAN 2025"
-        other="
-            A Harry Potter lore database interface made in React and Tailwind.
-						Fetches from the unofficial PotterDB API for all relevant information.
-						Utilizes Axios to implement request handling and data management.
-						Uses routing library React Router for slug routing and context keeping, and component library Shadcn to speed up the designing process.
-						Utilizes CSS to implement mobile-responsiveness.
-						Uses JavaScript to implement additional functionality and animation not covered by external libraries.
-          "
-        techInvolved={ ["simple-icons:React", "simple-icons:Axios", "simple-icons:TailwindCSS"] }
-      /> 
-      <Article 
-        id="proj3"
-        href="https://github.com/HoweZae/portfolio"
-        title="JoseTomanan.io"
-        subtitle="Personal portfolio-resume (i.e., this webpage!)"
-        date="JUL 2024 - PRESENT"
-        other="
-            A static single-page website that showcases the relevant skills and experiences I have earned throughout my learning journey.
-            Also created to demonstrate frontend tech learned and to practice my UI/UX design choices.
-            Written using Svelte and Tailwind to prioritize the webpage's responsiveness and intuitiveness.
-            Utilizes icon library Iconify and component library Shadcn to enhance the UI.
-          "
-        techInvolved={ ["simple-icons:Svelte", "simple-icons:TailwindCSS", "simple-icons:PostCSS"] }
-      />
-      <Article 
-        id="proj0"
-        href="https://github.com/JoseTomanan/shogi-pygame"
-        title="Shogimon"
-        subtitle="A Pokemon-inspired Shogi-esque video game using Python"
-        date="DEC 2024 - JAN 2025"
-        other="
-            A Model-View-Controller (MVC) structured simple video game created using Python video game package Pygame.
-            Employs collaboration-ready programming practices such as the SOLID principles, design patterns, and message passing between components.
-            Implements both online (networking) and offline (pass-and-play) mode.
-            Made in compliance for the requirements of course CS 150: Programming Languages.
-          "
-        techInvolved={ ["simple-icons:Python", "simple-icons:Go"] }
-      />
-      <Article 
-        id="proj2"
-        href="https://tes-birth-rate.vercel.app"
-        title="Poverty and Parenting: A Data Science Study"
-        subtitle="Economic analysis of birth rates in the Philippines"
-        date="FEB 2024 - JUN 2024"
-        other="
-            A visual exploration study, done using data science Python packages: Pandas, Seaborn, Geopandas.
-            Uses data science concepts to find an association between registered live births and poverty incidence in each municipality, as well as data visualization techniques to make the results intuitive and easily digestible.
-            Conducted in compliance for the requirements of course CS 132: Data Science.
-          "
-        techInvolved={ ["simple-icons:Python"] }
-      />
-      <!-- <Article 
-        id="proj1"
-        href="https://github.com/JoseTomanan/finance-tracker-automator"
-        title="Finance Tracker Automator"
-        subtitle="Automation scripts for Google Sheets using AppsScript"
-        date="FEB 2023 - PRESENT"
-        other="
-            A scripting project that I use for my personal spreadsheet to track my daily outgoing and incoming funds.
-            Uses the API to automate the addition of new entries, separation of each week, and creation of a new sheet for each new month.
-            Written in TypeScript, later transpiled into JavaScript through Clasp (for Google AppsScript).
-            Implements SOLID principles and design patterns for codebase sustainability and maintainability.
-          "
-        techInvolved={ ["simple-icons:GoogleAppsScript", "simple-icons:TypeScript"] }
-      /> -->
+      <Article title="Lutong Bahay"
+						id="proj-192"
+						href="https://github.com/JoseTomanan/cs192-lutong-bahay"
+						subtitle="Full-stack app/database for Filipino recipes"
+						date="AUG 2024 - MAY 2025"
+						other="
+								A database web app with fully functional search filters/sorting functions and user authentication system.
+								Also includes an administrator mode for adding/editing recipes and suspending users.
+								Written using Svelte and Tailwind (frontend), and Django-REST framework (backend), with the database hosted in Supabase.
+								Part of a 5-man, year long project done in compliance for the requirements of course CS 191 and 192: Software Engineering I and II."
+						techInvolved={ ["simple-icons:Svelte", "simple-icons:TailwindCSS", "simple-icons:Django", "simple-icons:Supabase"] }
+					/>
+			<Article title="Potterdex"
+						id="proj-potterdex"
+						href="https://potterdex.vercel.app"
+						subtitle="React-based Harry Potter database interface"
+						date="DEC 2024 - JAN 2025"
+						other="
+								A Harry Potter lore database interface made in React and Tailwind.
+								Fetches from the unofficial PotterDB API for all relevant information.
+								Utilizes Axios to implement request handling and data management.
+								Uses routing library React Router for slug routing and context keeping, and component library Shadcn to speed up the designing process.
+								Utilizes CSS to implement mobile-responsiveness.
+								Uses JavaScript to implement additional functionality and animation not covered by external libraries."
+						techInvolved={ ["simple-icons:React", "simple-icons:Axios", "simple-icons:TailwindCSS"] }
+					/>
+      <Article title="JoseTomanan.io"
+						id="proj-portfolio"
+						href="https://github.com/HoweZae/portfolio"
+						subtitle="Personal portfolio-resume (i.e., this webpage!)"
+						date="JUL 2024 - PRESENT"
+						other="
+								A static single-page website that showcases the relevant skills and experiences I have earned throughout my learning journey.
+								Also created to demonstrate frontend tech learned and to practice my UI/UX design choices.
+								Written using Svelte and Tailwind to prioritize the webpage's responsiveness and intuitiveness.
+								Utilizes icon library Iconify and component library Shadcn to enhance the UI."
+						techInvolved={ ["simple-icons:Svelte", "simple-icons:TailwindCSS", "simple-icons:PostCSS"] }
+					/>
+      <Article title="Shogimon"
+						id="proj-150"
+						href="https://github.com/JoseTomanan/shogi-pygame"
+						subtitle="A Pokemon-inspired Shogi-esque video game using Python"
+						date="DEC 2024 - JAN 2025"
+						other="
+								A Model-View-Controller (MVC) structured simple video game created using Python video game package Pygame.
+								Employs collaboration-ready programming practices such as the SOLID principles, design patterns, and message passing between components.
+								Implements both online (networking) and offline (pass-and-play) mode.
+								Made in compliance for the requirements of course CS 150: Programming Languages."
+						techInvolved={ ["simple-icons:Python", "simple-icons:Go"] }
+					/>
+      <Article title="Poverty and Parenting: A Data Science Study"
+						id="proj-132"
+						href="https://tes-birth-rate.vercel.app"
+						subtitle="Economic analysis of birth rates in the Philippines"
+						date="FEB 2024 - JUN 2024"
+						other="
+								A visual exploration study, done using data science Python packages: Pandas, Seaborn, Geopandas.
+								Uses data science concepts to find an association between registered live births and poverty incidence in each municipality, as well as data visualization techniques to make the results intuitive and easily digestible.
+								Conducted in compliance for the requirements of course CS 132: Data Science."
+						techInvolved={ ["simple-icons:Python"] }
+					/>
+      <!-- <Article title="Finance Tracker Automator"
+						id="proj-finances"
+						href="https://github.com/JoseTomanan/finance-tracker-automator"
+						subtitle="Automation scripts for Google Sheets using AppsScript"
+						date="FEB 2023 - PRESENT"
+						other="
+								A scripting project that I use for my personal spreadsheet to track my daily outgoing and incoming funds.
+								Uses the API to automate the addition of new entries, separation of each week, and creation of a new sheet for each new month.
+								Written in TypeScript, later transpiled into JavaScript through Clasp (for Google AppsScript).
+								Implements SOLID principles and design patterns for codebase sustainability and maintainability.
+							"
+						techInvolved={ ["simple-icons:GoogleAppsScript", "simple-icons:TypeScript"] }
+					/> -->
     </div>
     
     <h6 class="footnote">
--- src/routes/Roles.svelte
+++ src/routes/Roles.svelte
@@ -11,60 +11,59 @@
     
     <div class="flex flex-col gap-6">
       <Article 
-        id="role4"
-        href="https://www.linkedin.com/in/jedtomanan/details/experience/"
-        title="Software Engineer Intern"
-        subtitle="Pointwest Innovations Corporation"
-        date="JUN 2025 - PRESENT"
-        other="
-            Took part in a 200-hour software engineering internship program.
-            Tasked in a 10-man case study for developing an HR-based application for the company.
-            Part of the backend team: built the API endpoints using Spring Boot.
-            Also developed testing suites to assure the quality of the application during the development, refactoring, and maintenance process.
-          "
-        techInvolved={ ["simple-icons:Spring", "simple-icons:Git", "simple-icons:Jira"] }
-      />
+						id="role-195"
+						title="Software Engineer Intern"
+						href="https://www.linkedin.com/in/jedtomanan/details/experience/"
+						subtitle="Pointwest Innovations Corporation"
+						date="JUN 2025 - PRESENT"
+						other="
+								Took part in a 200-hour software engineering internship program.
+								Tasked in a 10-man case study for developing an HR-based application for the company.
+								Part of the backend team: built the API endpoints using Spring Boot.
+								Also developed testing suites to assure the quality of the application during the development, refactoring, and maintenance process.
+							"
+						techInvolved={ ["simple-icons:Spring", "simple-icons:Git", "simple-icons:Jira"] }
+					/>
       <Article 
-        id="role3"
-        href="https://www.linkedin.com/in/jedtomanan/details/experience/"
-        title="Tackle Junior Head"
-        subtitle="UP Association of Computer Science Majors (UP CURSOR)"
-        date="SEP 2023 - MAR 2024"
-        other="
-            Co-headed a team of 20 in planning, marketing, and promoting a 4-day hybrid setup tech career fair.
-            Lead the Branding & Creatives team to effectively design and market the event.
-            Lead the Human Resources to internally disseminate the event.
-            Used Notion workspace to manage the project through task delegation, resource management, and team oversight.
-          "
-        techInvolved={ ["simple-icons:Notion", "simple-icons:Asana", "simple-icons:GoogleSheets", "simple-icons:Canva"] }
-      />
+						id="role-cursor1"
+						href="https://www.linkedin.com/in/jedtomanan/details/experience/"
+						title="Tackle Junior Head"
+						subtitle="UP Association of Computer Science Majors (UP CURSOR)"
+						date="SEP 2023 - MAR 2024"
+						other="
+								Co-headed a team of 20 in planning, marketing, and promoting a 4-day hybrid setup tech career fair.
+								Lead the Branding & Creatives team to effectively design and market the event.
+								Lead the Human Resources to internally disseminate the event.
+								Used Notion workspace to manage the project through task delegation, resource management, and team oversight.
+							"
+						techInvolved={ ["simple-icons:Notion", "simple-icons:Asana", "simple-icons:GoogleSheets", "simple-icons:Canva"] }
+					/>
       <Article 
-        id="role2"
-        href="https://www.linkedin.com/in/jedtomanan/details/experience/"
-        title="Dev Training Program Contributor"
-        subtitle="UP Center for Student Innovations (UP CSI)"
-        date="SEP 2023 - NOV 2023"
-        other="
-            Wrote web development modules and exercises designed for 50+ UP CSI applicants, for app process 2324B.
-            Used Typst markdown to automate structure, layoutting, and organization of module content.
-            Collaborated using Git to streamline team workflow and consolidate collective output.
-          "
-        techInvolved={ ["simple-icons:Typst", "simple-icons:Git"] }
-      />
+						id="role-csi"
+						href="https://www.linkedin.com/in/jedtomanan/details/experience/"
+						title="Dev Training Program Contributor"
+						subtitle="UP Center for Student Innovations (UP CSI)"
+						date="SEP 2023 - NOV 2023"
+						other="
+								Wrote web development modules and exercises designed for 50+ UP CSI applicants, for app process 2324B.
+								Used Typst markdown to automate structure, layoutting, and organization of module content.
+								Collaborated using Git to streamline team workflow and consolidate collective output.
+							"
+						techInvolved={ ["simple-icons:Typst", "simple-icons:Git"] }
+					/>
       <Article 
-        id="role1"
-        href="https://www.linkedin.com/in/jedtomanan/details/experience/"
-        title="Member Relations Head"
-        subtitle="UP CURSOR"
-        date="FEB 2023 - MAY 2023"
-        other="
-            Headed a team of 6 in assuring and maintaining member involvement & motivation of an organization with 100+ active members.
-            Created publication material for CURSOR Cares, to encourage organization interaction.
-            Published material for Humans of CURSOR to share the experience of members in their respective fields.
-            Used Notion workspace to efficiently implement task delegation, resource management, and team oversight.
-          "
-        techInvolved={ ["simple-icons:Notion"] }
-      />
+						id="role-cursor2"
+						href="https://www.linkedin.com/in/jedtomanan/details/experience/"
+						title="Member Relations Head"
+						subtitle="UP CURSOR"
+						date="FEB 2023 - MAY 2023"
+						other="
+								Headed a team of 6 in assuring and maintaining member involvement & motivation of an organization with 100+ active members.
+								Created publication material for CURSOR Cares, to encourage organization interaction.
+								Published material for Humans of CURSOR to share the experience of members in their respective fields.
+								Used Notion workspace to efficiently implement task delegation, resource management, and team oversight."
+						techInvolved={ ["simple-icons:Notion"] }
+					/>
     </div>
     
     <h6 class="footnote">
