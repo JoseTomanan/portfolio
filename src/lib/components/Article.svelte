f0a71a17ce8289286424f8b70ee94691f8ba43ac
JoseTomanan <jdtomanan@up.edu.ph>
2026-04-08 22:29:46 +0800

    refactor: Clean up formatting and indentation in Article.svelte

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -12,29 +12,30 @@
   export let techInvolved: string[] = [""];
 </script>
 
-<article class="group/whole flex flex-col md:flex-row gap-4 items-baseline p-4 rounded-lg text-left
-			hover:bg-muted z-10
-			shadow-md shadow-border">
+<article class="group/whole
+      flex flex-col md:flex-row gap-4 items-baseline p-4 rounded-lg text-left
+      bg-muted/30 hover:bg-muted z-10
+      border border-border/10">
   <div class="flex flex-row w-full justify-between gap-0.5
-				md:basis-1/4 md:flex-col">
+        md:basis-1/4 md:flex-col">
     <span class="text-left md:text-right text-sm font-mono tracking-tighter w-full
-					text-muted-foreground group-hover/whole:text-foreground/95">
+          text-muted-foreground group-hover/whole:text-foreground/95">
       {date}
     </span>
 
     <div class="flex flex-row justify-end gap-2">
       {#each techInvolved as tech}
-				{@const actualTag = tech.toLowerCase()}
-				{@const displayableName = tech.split(":")[1]}
-				<Tooltip.Root>
-					<Tooltip.Trigger class="items-center" aria-label={displayableName}>
-						<Icon icon={actualTag}
-									class="h-4 w-4 text-muted-foreground/80 group-hover/whole:text-muted-foreground cursor-default" />
-					</Tooltip.Trigger>
-					<Tooltip.Content class="bg-background/85 text-foreground border-none">
-						{displayableName}
-					</Tooltip.Content>
-				</Tooltip.Root>
+        {@const actualTag = tech.toLowerCase()}
+        {@const displayableName = tech.split(":")[1]}
+        <Tooltip.Root>
+          <Tooltip.Trigger class="items-center" aria-label={displayableName}>
+            <Icon icon={actualTag}
+                  class="h-4 w-4 text-muted-foreground/80 group-hover/whole:text-muted-foreground cursor-default" />
+          </Tooltip.Trigger>
+          <Tooltip.Content class="bg-background/85 text-foreground border-none">
+            {displayableName}
+          </Tooltip.Content>
+        </Tooltip.Root>
       {/each}
     </div>
   </div>
@@ -43,8 +44,8 @@
     <div class="md:grid space-y-1">
       <h4 class="underline-offset-2 flex font-sans font-semibold group-hover/whole:text-foreground/100">
         <a class="group/link flex items-baseline leading-5 gap-2 hover:gap-4
-							text-foreground/90 hover:text-secondary"
-							target="_blank" rel="noopener noreferrer" {id} {href} on:click|once>
+              text-foreground/90 hover:text-secondary"
+              target="_blank" rel="noopener noreferrer" {id} {href} on:click|once>
           {title}
           <Icon icon="ic:baseline-arrow-outward"
               class="w-4 h-4 opacity-0 group-hover/whole:opacity-100 hidden md:inline"/>
@@ -56,7 +57,7 @@
     </div>
 
     <p class="grow leading-5 tracking-tighter
-					text-muted-foreground/75 group-hover/whole:text-muted-foreground/100">
+          text-muted-foreground/75 group-hover/whole:text-muted-foreground/100">
       {other}
     </p>
   </div>
