82ff4cbad1c2cbc962e15eb2254b14385559344c
JoseTomanan <jdtomanan@up.edu.ph>
2026-04-09 10:59:54 +0800

    refactor: Move boilerplatey stuff to layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,16 +14,6 @@
 </script>
 
 
-<svelte:head>
-  <title>JoseTomanan.io</title>
-  <meta name="description" content="Jose's portfolio" />
-  <meta property="og:image" content="https://josetmnn.vercel.app/cover.JPG" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-  <link rel="manifest" href="/site.webmanifest">
-</svelte:head>
-
 
 <Top />
 
