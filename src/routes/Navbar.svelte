493757861f5ae92e02f15e6754c438f55c3277e3
JoseTomanan <jdtomanan@up.edu.ph>
2026-04-10 01:09:16 +0800

    style(nav): refine CV link icon positioning

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -13,11 +13,14 @@
 	<a href="#me">Me</a>
 	<a href="#roles">Roles</a>
 	<a href="#projects">Projects</a>
-	<a id="clickable-resume" href={ link.cv } target="_blank" rel="noopener noreferrer"
+	<a id="clickable-resume"
+        href={ link.cv }
+        target="_blank"
+        rel="noopener noreferrer"
 				class="relative group/resume flex flex-row items-baseline gap-1 w-fit">
 		<span>CV</span>
 		<Icon icon="ic:outline-arrow-outward"
-					class="absolute top-0.5 -translate-x-1/2 -right-5 size-4 opacity-0 group-hover/resume:opacity-100" />
+					class="absolute top-2.5 -right-5 size-4 opacity-0 group-hover/resume:opacity-100" />
 	</a>
 		<!-- <a href="#contact">Contact</a> -->
 </nav>
