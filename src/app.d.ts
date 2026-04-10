5e70faefec82d701ee3c2f4c9d893169ffa5d1f1
JoseTomanan <jdtomanan@up.edu.ph>
2026-04-10 22:02:15 +0800

    build: configure unplugin-icons for Svelte

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="@sveltejs/enhanced-img" />
+/// <reference types="unplugin-icons/types/svelte" />
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
   plugins: [
     enhancedImages(),
     sveltekit(),
+    Icons({ compiler: 'svelte' }),
   ],
   server: {
     port: 1234
