07141e49f0dc23d9df2ca2df9b4d187008be1c86
JoseTomanan <jdtomanan@up.edu.ph>
2026-04-10 12:42:13 +0800

    chore: Minor

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,7 +2,7 @@ export const link = {
     li: "https://www.linkedin.com/in/jedtomanan/",
     gh: "https://github.com/JoseTomanan",
     fb: "https://www.facebook.com/jedtomanan/",
-    ig: "https://www.instagram.com/jose.tmnn/",
+    ig: "https://www.instagram.com/josetmnn/",
     ig_swiftie: "https://www.instagram.com/p/B6p4rR_hoUC/",
     twt: "https://x.com/howe_zae",
     resume: "./documents/resume.pdf",
