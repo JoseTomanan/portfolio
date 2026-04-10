092ed0dc9bb2848415bce08a9ec16d0d5b6b87c5
JoseTomanan <jdtomanan@up.edu.ph>
2026-04-10 23:58:50 +0800

    Refactor navbar layout

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { link } from "$lib";
-  import IcOutlineArrowOutward from '~icons/ic/outline-arrow-outward';
 
   let scrollY = $state(0);
 
@@ -10,28 +9,38 @@
 
 <svelte:window bind:scrollY />
 
-<nav aria-label="Main navigation"
-      style="top: {navTop}px;"
-      class="bg-background/50 text-foreground
-              flex flex-row items-center justify-center align-middle backdrop-blur-sm
-              fixed left-1/2 -translate-x-1/2 w-fit z-50 gap-6 sm:gap-10
-              px-8 rounded-full ring ring-border/20
-              shadow shadow-border/50
-              *:py-2 *:font-medium
+<div style="top: {navTop}px;"
+      class="fixed left-1/2 -translate-x-1/2 z-50 flex flex-row items-center gap-4
+          [&>nav]:bg-background/50 [&>nav]:text-foreground [&>nav]:backdrop-blur-sm
+          [&>nav]:rounded-full
+          [&>nav]:ring [&>nav]:ring-border/20 [&>nav]:w-fit
+          [&>nav]:shadow [&>nav]:shadow-border/50
+          [&>nav]:flex [&>nav]:flex-row [&>nav]:items-center [&>nav]:align-middle
+          [&>nav]:font-medium
+          [&>nav>*]:py-2
+        ">
+  <nav aria-label="Main navigation"
+        class="justify-center gap-6 px-8 sm:gap-10
               *:hover:text-primary
               *:focus-visible:text-primary *:focus-visible:underline
             ">
-  <a href="#top">Me</a>
-  <a href="#roles">Roles</a>
-  <a href="#projects">Projects</a>
-  <a id="clickable-resume"
-      href={ link.cv }
-      target="_blank"
-      rel="noopener noreferrer"
-      class="relative group/resume flex flex-row items-baseline gap-1 w-fit">
-    <span>CV</span>
-    <IcOutlineArrowOutward
-          class="absolute top-2.5 -right-5 size-4 opacity-0 group-hover/resume:opacity-100" />
-  </a>
+    <a href="#top">Me</a>
+    <a href="#roles">Roles</a>
+    <a href="#projects">Projects</a>
     <!-- <a href="#contact">Contact</a> -->
-</nav>
+  </nav>
+
+  <nav aria-label="Resume navigation"
+        class="px-6">
+    <a id="clickable-resume"
+        href={link.cv}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="flex flex-row items-center gap-1 underline-offset-2
+              hover:text-secondary hover:underline
+              focus-visible:text-secondary focus-visible:underline
+            ">
+      <span>CV</span>
+    </a>
+  </nav>
+</div>
