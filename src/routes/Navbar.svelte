2780eb2ad09b6fcf9aa3870ed136aba636b50f62
HoweZae <jdtomanan@up.edu.ph>
2026-04-11 23:39:42 +0800

    style: Make 1 sliced pill instead of 2 separate pills

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -11,9 +11,8 @@
 <svelte:window bind:scrollY />
 
 <div style="top: {navTop}px;"
-      class="fixed left-1/2 -translate-x-1/2 z-50 flex flex-row items-center gap-4
+      class="fixed left-1/2 -translate-x-1/2 z-50 flex flex-row items-center gap-x-px
           [&>nav]:bg-background/50 [&>nav]:text-foreground [&>nav]:backdrop-blur-sm
-          [&>nav]:rounded-full
           [&>nav]:ring [&>nav]:ring-border/20 [&>nav]:w-fit
           [&>nav]:shadow [&>nav]:shadow-border/50
           [&>nav]:flex [&>nav]:flex-row [&>nav]:items-center [&>nav]:align-middle
@@ -23,6 +22,7 @@
   <nav aria-label="Main navigation"
         style="padding-block: {navVerticalPadding}px;"
         class="justify-center gap-6 px-8 sm:gap-10
+              rounded-l-full
               *:hover:text-primary
               *:focus-visible:text-primary *:focus-visible:underline
             ">
@@ -34,7 +34,7 @@
 
   <nav aria-label="Resume navigation"
         style="padding-block: {navVerticalPadding}px;"
-        class="px-6">
+        class="px-6 rounded-r-full">
     <a id="clickable-resume"
         href={link.cv}
         target="_blank"
